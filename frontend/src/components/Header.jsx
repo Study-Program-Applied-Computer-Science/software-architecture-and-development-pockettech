b45a2fb3680d5fd3fca282383941247d0bb29371
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2025-01-25 16:37:43 +0100

    comments

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,14 +5,13 @@ import { FaRegMoon } from "react-icons/fa";
 const Header = ({ isDarkMode, themeSwitch }) => {
   return (
     <header className="dark:bg-gray-800">
-      {/* Button for theme toggle */}
+      {/* Button for dark and light mode toggle */}
       <button
   onClick={themeSwitch}
   className={`absolute top-4 right-4 flex items-center justify-center w-10 h-10 rounded-full transition-colors ${
     isDarkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-200 hover:bg-gray-300'
   }`}
 >
-  {/* Change icon size and color based on the theme */}
   {isDarkMode ? (
     <FaRegMoon size={20} className="text-white" />
   ) : (
