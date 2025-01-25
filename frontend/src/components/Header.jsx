7f9dea9a67cfdc16ba46a05b8ccca407cbdf46ff
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2025-01-25 16:34:25 +0100

    Added header component with light and dark mode switch and also removed placeholder

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { CiSun } from "react-icons/ci";
+import { FaRegMoon } from "react-icons/fa";
+
+const Header = ({ isDarkMode, themeSwitch }) => {
+  return (
+    <header className="dark:bg-gray-800">
+      {/* Button for theme toggle */}
+      <button
+  onClick={themeSwitch}
+  className={`absolute top-4 right-4 flex items-center justify-center w-10 h-10 rounded-full transition-colors ${
+    isDarkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-200 hover:bg-gray-300'
+  }`}
+>
+  {/* Change icon size and color based on the theme */}
+  {isDarkMode ? (
+    <FaRegMoon size={20} className="text-white" />
+  ) : (
+    <CiSun size={20} className="text-black" />
+  )}
+</button>
+
+    </header>
+  );
+};
+
+export default Header;
