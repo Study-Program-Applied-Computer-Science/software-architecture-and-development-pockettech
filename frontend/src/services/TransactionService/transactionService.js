bc90bdc2c6b59e4a10fad8f5f7f10b49a60c2a9e
sriraminhdb <166843052+sriraminhdb@users.noreply.github.com>
2025-02-02 23:39:50 +0100

    fix: Change in Port Number

--- frontend/src/services/TransactionService/transactionService.js
+++ frontend/src/services/TransactionService/transactionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/transactions';
+const API_URL = 'http://localhost:8005/transactions';
 
 export const getTransactions = async () => {
     try {
